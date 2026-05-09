3ead6092539385d331f3c2b088d6ed7c8d2b69ef
unknown <samir@falconflh.com>
2026-05-09 16:20:02 +0300

    add breeze login and authintication

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use  App\Http\Controllers\ActivityLogController;
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/logs', [ActivityLogController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/test-mail', function () {
+    Mail::raw('Hello', function ($message) {
+        $message->to('test@test.com')
+                ->subject('Test');
+    });
+
+    return 'sent';
+});
+require __DIR__.'/auth.php';
