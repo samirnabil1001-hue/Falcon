9e598dc511b5b1374d9fb15efe41ebe99e294e5a
samir.nabil <samir.nabil1001@gmail.com>
2026-05-31 17:58:21 +0300

    update the view

--- app/Enums/CompanyService.php
+++ app/Enums/CompanyService.php
@@ -5,12 +5,12 @@ namespace App\Enums;
 enum CompanyService: string
 {
     case FLIGHTS = 'flights';
+    case HOTEL_BOOKINGS = 'hotel_bookings';
     case TOURIST_VISAS = 'tourist_visas';
     case FREE_RESIDENCIES = 'free_residencies';
-    case HOTEL_BOOKINGS = 'hotel_bookings';
     case TRAVEL_PACKAGES = 'travel_packages';
     case TURKEY = 'turkey';
-    case TRAVEL_PERMITS = 'travel_permits';
+    case OTHERS = 'other';
 
     public function label(): string
     {
@@ -19,9 +19,10 @@ enum CompanyService: string
             self::TOURIST_VISAS => 'تأشيرات سياحية',
             self::FREE_RESIDENCIES => 'إقامات حرة',
             self::HOTEL_BOOKINGS => 'حجز فنادق',
-            self::TRAVEL_PACKAGES => 'باقات سفر ',
-            self::TURKEY => ' سفر تركيا',
-            self::TRAVEL_PERMITS => 'تصريح سفر',
+            self::TRAVEL_PACKAGES => 'برامج سياحية ',
+            self::TURKEY => ' تشغيل عمالة',
+            self::OTHERS => 'اخري',
+
         };
     }
 }
